bb939452ffca25bb1286b5941c56668835d15161
Divilaksh <llakshmimani101010@gmail.com>
2022-01-07 21:12:27 -0600

    Done

---
+++ SuperMarket.java
@@ -0,0 +1,18 @@
+public class SuperMarket
+{
+static String name = "laksh";
+static int doorNo = 1234;
+int price,discount;
+String brand;
+public static void main (String[] args)
+{
+SuperMarket prod1 = new SuperMarket();
+prod1.price = 100;
+prod1.discount = 20;
+prod1.brand = "ABCD";
+SuperMarket prod2 = new SuperMarket();
+prod2.price = 200;
+prod2.discount = 30;
+prod2.brand = "BCD";
+}
+}
